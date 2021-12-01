d5b78734a4cd12ba8202d760028c5c87c123a050
TristanC98 <91906118+TristanC98@users.noreply.github.com>
2021-12-01 10:17:40 +0100

    setup

--- leaderboard.php
+++ leaderboard.php
@@ -7,15 +7,32 @@
   <link ref="stylesheet" href="css/main.css">
   <link ref="stylesheet" href="css/reset.css">
   <title>Leaderboards</title>
-</head>
-<body>
   <?php
     // require_once header.php 
     ?>
+  <?php
+    // require_once footer.php
+    ?>
+</head>
+<body>
+  <?php
+     
+    ?>
     <div class=" container">
-    <h1 class="container__title"> Leaderboards </h1>
+    <h1 class="container__title"> Leaderboard </h1>
     <main class ="leaderboard-container">
-      
+    <ul class="leaderboard-container__score">
+      <li> placeholder 1</li>
+      <li> placeholder 2</li>
+      <li> placeholder 3</li>
+      <li> placeholder 4</li>
+      <li> placeholder 5</li>
+      <li> placeholder 6</li>
+      <li> placeholder 7</li>
+      <li> placeholder 8</li>
+      <li> placeholder 9</li>
+      <li> placeholder 10</li>
+    </ul>
 
 
     </main>
@@ -23,8 +40,6 @@
 
 
 
-    <?php
-    // require_once footer.php
-    ?>
+   
 </body>
 </html>
