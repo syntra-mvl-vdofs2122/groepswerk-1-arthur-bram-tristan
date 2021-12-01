02e5c2b1d3143ed9fc3ee274860483ead32d3c68
TristanC98 <91906118+TristanC98@users.noreply.github.com>
2021-12-01 10:02:35 +0100

    leaderboard setup

---
+++ leaderboard.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link ref="stylesheet" href="css/main.css">
+  <link ref="stylesheet" href="css/reset.css">
+  <title>Leaderboards</title>
+</head>
+<body>
+  <?php
+    // require_once header.php 
+    ?>
+    <div class=" container">
+    <h1 class="container__title"> Leaderboards </h1>
+    <main class ="leaderboard-container">
+      
+
+
+    </main>
+    </div>
+
+
+
+    <?php
+    // require_once footer.php
+    ?>
+</body>
+</html>
