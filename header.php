37463afa60145c97dcc2bbd22ca831789af1a694
Bram Dieudonne <dieudonnebram28@gmail.com>
2021-12-04 18:33:21 +0100

    added basic header code

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="header">
+	<nav class="header__navigation">
+		<a href="index.php" class="header__navigation-item">index</a>
+		<a href="question.php" class="header__navigation-item">Quiz</a>
+		<a href="leaderboard.php" class="header__navigation-item">Leaderboard</a>
+		<a href="info.php" class="header__navigation-item">Info</a>
+
+	</nav>
+</header>
