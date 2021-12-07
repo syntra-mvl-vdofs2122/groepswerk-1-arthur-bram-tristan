62a1d22d965588c0b66a3e1b67e6ec4b7d37900e
Arthur Lambrechts <arthur.lambrechts.al@gmail.com>
2021-12-07 20:39:22 +0100

    basic css & html

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-<link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-  
+  <main class="slogan">
+  <span class="slogan__text">#teamseas. Save the ocean!</span>
+  <form class="" action="input" method="post">
+    <button type="submit"> Take the quiz </button>
+  </form>
+  </main>
 </body>
 </html>
