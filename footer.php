44b243072b50ffc662acb14e3a85b8baf56dbae0
Arthur Lambrechts <arthur.lambrechts.al@gmail.com>
2021-12-07 20:49:08 +0100

    footer basic html

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+	<div class="footer-container">
+		<ul class="footer-container__socials">
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+			<li><a href=""></a></li>
+		</ul>
+	</div>
+</footer>
