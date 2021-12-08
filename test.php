454e5055dafafd1c3e1a0f7641775cebc96ca4d1
TristanC98 <91906118+TristanC98@users.noreply.github.com>
2021-12-08 10:34:44 +0100

    leaderboard

--- test.php
+++ test.php
@@ -5,10 +5,16 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-  
+  <ul>
+    <li class="item"></li>
+    <li class="item"></li>
+    <li class="item"></li>
+    <li class="item"></li>
+    <li class="item"></li>
+  </ul>
+
+  </ul>
 </body>
 </html>
