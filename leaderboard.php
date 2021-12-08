21184639ceff193adbc1678a053b69387a348904
TristanC98 <91906118+TristanC98@users.noreply.github.com>
2021-12-08 10:33:09 +0100

    features_leaderboard_design

--- leaderboard.php
+++ leaderboard.php
@@ -1,45 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link ref="stylesheet" href="css/main.css">
-  <link ref="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/styles.css">
   <title>Leaderboards</title>
+
   <?php
-    // require_once header.php 
-    ?>
-  <?php
-    // require_once footer.php
-    ?>
+  // require_once header.php
+  // require_once footer.php
+  ?>
+
+
 </head>
+
 <body>
-  <?php
-     
-    ?>
-    <div class=" container">
-    <h1 class="container__title"> Leaderboard </h1>
-    <main class ="leaderboard-container">
-    <ul class="leaderboard-container__score">
-      <li> placeholder 1</li>
-      <li> placeholder 2</li>
-      <li> placeholder 3</li>
-      <li> placeholder 4</li>
-      <li> placeholder 5</li>
-      <li> placeholder 6</li>
-      <li> placeholder 7</li>
-      <li> placeholder 8</li>
-      <li> placeholder 9</li>
-      <li> placeholder 10</li>
-    </ul>
+  <main class="leaderboard">
+    <h1 class="leaderboard__title">Leaderboard</h1>
 
-
-    </main>
+    <div class="leaderboard__legend-container legend">
+      <h2 class="legend__name"> Name </h2>
+      <img class="legend__logo" src="https://upload.wikimedia.org/wikipedia/commons/2/25/Teamseas-logo.png" alt="teamseas-logo">
+      <h2 class="legend__score"> Score </h2>
     </div>
 
-
-
-   
+    <div class="leaderboard__ranking-container ranking">
+      <ol class="ranking__player-list ">
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>
+        <li class="ranking__player-item"> Player </li>     
+      </ol>
+      <ol class="ranking__score-list">
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+        <li class="ranking__score-item"> score </li>
+      </ol>
+    </div>
+  </main>
 </body>
+
+</html>
+
 </html>
