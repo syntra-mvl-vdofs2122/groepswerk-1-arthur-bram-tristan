6c63be3ad5bbd9d347c548ccae0d211354da12f5
Bram Dieudonne <dieudonnebram28@gmail.com>
2021-12-14 22:37:55 +0100

    started with functionality

---
+++ assets/header.php
@@ -0,0 +1,25 @@
+<header class="header">
+	<nav class="header__navigation">
+		<a href="index.php" class="header__navigation-item">index</a>
+		<a href="question.php" class="header__navigation-item">Quiz</a>
+		<a href="leaderboard.php" class="header__navigation-item">Leaderboard</a>
+		<a href="info.php" class="header__navigation-item">Info</a>
+
+	</nav>
+	<form action="" method="POST">
+	<input type="submit" value="Clear username" name="clearname">
+	<input type="submit" value="Clear all" name="clearall">
+	</form>
+	<?php
+   
+
+	 if(isset($_POST['clearname'])) {
+		unset($_SESSION['name']);
+	}
+	if(isset($_POST['clearall'])) {
+		unset($_SESSION['name']);
+		unset($_SESSION['answer1']);
+		unset($_SESSION['rightanswers']);
+	}
+?>
+</header>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,19 +11,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
 <link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/master.css">
 <link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
   <?php
-  include 'header.php';
+	include 'assets/header.php';
   ?>
   <main class="slogan">
   <span class="slogan__text">#teamseas. Save the ocean!</span>
   <form class="" action="input" method="post">
     <button type="submit"> Take the quiz </button>
   </form>
+  <a href="question.php">take the quiz</a>
   </main>
 </body>
 
