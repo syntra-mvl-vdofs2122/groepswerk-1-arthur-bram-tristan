9f4d275841b1bdd26281796241ae1205ef568362
Arthur Lambrechts <arthur.lambrechts.al@gmail.com>
2021-12-15 10:09:25 +0100

    initial commit

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/main.css">
-<link rel="stylesheet" href="css/index.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="scss/master.css">
+  <link rel="stylesheet" href="scss/index.css">
 </head>
 
 <body>
