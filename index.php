f0c4e346b6dd6775a69b9209dc6a3bdbab2851d1
ALambrechts <94166654+ALambrechts@users.noreply.github.com>
2021-12-15 10:32:14 +0100

    Revert "Thematische CSS"

--- index.php
+++ index.php
@@ -10,10 +10,9 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="scss/master.css">
-  <link rel="stylesheet" href="scss/index.css">
+<link rel="stylesheet" href="css/reset.css">
+<link rel="stylesheet" href="css/master.css">
+<link rel="stylesheet" href="css/index.css">
 </head>
 
 <body>
