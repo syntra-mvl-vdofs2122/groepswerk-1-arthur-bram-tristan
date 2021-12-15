b741df13148566a830181d470e96d60370a735cb
Arthur Lambrechts <arthur.lambrechts.al@gmail.com>
2021-12-15 10:56:05 +0100

    Initial commit

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-session_start();
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,21 +6,21 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-<link rel="stylesheet" href="css/reset.css">
-<link rel="stylesheet" href="css/master.css">
-<link rel="stylesheet" href="css/index.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="scss/master.css">
+  <link rel="stylesheet" href="scss/index.css">
 </head>
 
 <body>
   <?php
-	include 'assets/header.php';
+  include 'header.php';
   ?>
   <main class="slogan">
   <span class="slogan__text">#teamseas. Save the ocean!</span>
   <form class="" action="input" method="post">
     <button type="submit"> Take the quiz </button>
   </form>
-  <a href="question.php">take the quiz</a>
   </main>
 </body>
 
